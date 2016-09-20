60495083863cd1373628350982a0376008d64eff
Ryan Vinyard and Satchel Grant <satchel@>
2016-09-20 14:56:50 -0700

    create backend for adding patients

--- src/main/java/Patient.java
+++ src/main/java/Patient.java
@@ -2,3 +2,77 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import org.sql2o.*;
+
+public class Patient {
+  private String name;
+  private int id;
+  private int doctorId;
+
+  public Patient(String name, int doctorId) {
+    this.name = name;
+    this.doctorId = doctorId;
+    this.save();
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getDoctorId() {
+    return doctorId;
+  }
+
+  public static List<Patient> all() {
+    String sql = "SELECT * FROM patient";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Patient.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherPatient) {
+    if (!(otherPatient instanceof Patient)) {
+      return false;
+    } else {
+      Patient newPatient = (Patient) otherPatient;
+      return this.getName().equals(newPatient.getName()) &&
+             this.getDoctorId().equals(newPatient.getDoctorId()) &&
+             this.getId().equals(newPatient.getDoctorId());
+
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO patient(name, doctorId) VALUES (:name, :doctorId)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .addParameter("doctorId", this.doctorId)
+        .executeUpdate().getKey();
+    }
+  }
+
+  public Patient find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      Patient patient = con.createQuery("SELECT * FROM patient where id=:id")
+        .addParameter("id", id)
+        .executeAndFetchFirst(Patient.class);
+      return patient;
+    }
+  }
+
+  public void updateName(String newName) {
+    this.name = newName;
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE patient SET name = :walkergirl WHERE id = :id";
+      con.createQuery(sql)
+        .addParameter("walkergirl", newName)
+        .addParameter("id", this.id)
+        .executeUpdate();
+    }
+  }
+}
